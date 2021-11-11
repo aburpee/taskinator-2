cc232b8f9d2d509d5c70212e33eac93f684636cb
bonobo <arburpee@gmail.com>
2021-11-11 00:20:20 -0800

    added preventDefault

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do")
+var formEl = document.querySelector("#task-form")
 
 var createTaskHandler = function () {
+
+    event.preventDefault()
+
     var listItemEl = document.createElement("li")
     listItemEl.className = "task-item"
     listItemEl.textContent = "Another Task"
     tasksToDoEl.appendChild(listItemEl)
+
+    
 }
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
 
 
